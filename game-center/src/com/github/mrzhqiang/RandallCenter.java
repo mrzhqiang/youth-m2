dafb4bc9e173f3caf965203c5ab7a673ddddfc5f
mrZQ <287431404@qq.com>
2018-03-01 00:30:40 +0800

    初始化仓库

---
+++ game-center/src/com/github/mrzhqiang/RandallCenter.java
@@ -0,0 +1,14 @@
+package com.github.mrzhqiang;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class RandallCenter {
+  private JPanel rootPanel;
+  private JTabbedPane contentTabbed;
+  private JPanel serverControlPanel;
+  private JPanel configGuidePanel;
+  private JPanel dataBackupPanel;
+  private JPanel dataCleanPanel;
+  private JTabbedPane configTabbed;
+}
