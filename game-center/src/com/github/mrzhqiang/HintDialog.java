b78607d184931740035444133ad4af3c514f48e1
mrZQ <287431404@qq.com>
2018-03-03 02:40:59 +0800

    初步完成UI样式，待实现逻辑

---
+++ game-center/src/com/github/mrzhqiang/HintDialog.java
@@ -0,0 +1,53 @@
+package com.github.mrzhqiang;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+public class HintDialog extends JDialog {
+  private JPanel contentPane;
+  private JButton buttonOK;
+  private JButton buttonCancel;
+
+  public HintDialog() {
+    setContentPane(contentPane);
+    setModal(true);
+    getRootPane().setDefaultButton(buttonOK);
+
+    buttonOK.addActionListener(e -> onOK());
+
+    buttonCancel.addActionListener(e -> onCancel());
+
+    // call onCancel() when cross is clicked
+    setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+    addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        onCancel();
+      }
+    });
+
+    // call onCancel() on ESCAPE
+    contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+        JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+  }
+
+  private void onOK() {
+    // add your code here
+    dispose();
+  }
+
+  private void onCancel() {
+    // add your code here if necessary
+    dispose();
+  }
+
+  public static void main(String[] args) {
+  }
+}
