4fad063e1bd431a3a9083ba82c83bef8541799f4
张强 <qiang.zhang@touchair.cn>
2019-07-04 13:06:01 +0800

    updated: all version to 1.0

---
+++ core/src/main/java/core/Core.java
@@ -0,0 +1,21 @@
+package core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Core
+ *
+ * @author qiang.zhang
+ */
+public class Core extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+
+  }
+}
